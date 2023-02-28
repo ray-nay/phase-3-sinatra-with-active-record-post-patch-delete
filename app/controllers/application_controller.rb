f803e57cdc62b77c2e042e16720c1879cab25109
Renee Mumbi <1723renee@gmail.com>
2023-03-01 02:24:44 +0300

    Done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,49 @@
+
+require 'pry'
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
-  get '/games' do
-    games = Game.all.order(:title).limit(10)
-    games.to_json
+  post '/reviews' do
+    review = Review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    )
+    review.to_json
   end
 
-  get '/games/:id' do
-    game = Game.find(params[:id])
 
-    game.to_json(only: [:id, :title, :genre, :price], include: {
-      reviews: { only: [:comment, :score], include: {
-        user: { only: [:name] }
-      } }
-    })
+  patch '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+    review.to_json
+  end
+  # get '/games' do
+  #   games = Game.all.order(:title).limit(10)
+  #   games.to_json
+  # end
+
+  # get '/games/:id' do
+  #   game = Game.find(params[:id])
+
+  #   game.to_json(only: [:id, :title, :genre, :price], include: {
+  #     reviews: { only: [:comment, :score], include: {
+  #       user: { only: [:name] }
+  #     } }
+  #   })
+  # end
+
+  delete '/reviews/:id' do
+    # find the review using the ID
+    review = Review.find(params[:id])
+    # delete the review
+    review.destroy
+    # send a response with the deleted review as JSON
+    review.to_json
   end
 
 end
